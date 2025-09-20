2a798579e75d3f08f564d9f86d257eab94c24fde
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-20 20:37:27 +0530

    Blueprint of the project

---
+++ Parking Management System.cpp
@@ -0,0 +1,13 @@
+//  ┌───────────────────────────────────────┐
+//  │ 1. Vehicle Entry → Assign Slot → Save │
+//  │ 2. Vehicle Exit  → Calc Charges → Free│
+//  │ 3. View Parked Vehicles               │
+//  │ 4. View Available Slots               │
+//  │ 5. Generate Report                    │
+//  │ 6. Exit Program
+
+ #include <iostream>
+ using namespace std;
+ int main(){
+    
+ }
