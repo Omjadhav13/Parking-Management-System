a8c98bf71f29689c425bde2230797f128ffd3594
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-20 21:22:58 +0530

    new update

--- Parking Management System.cpp
+++ Parking Management System.cpp
@@ -1,13 +1,71 @@
-//  ┌───────────────────────────────────────┐
-//  │ 1. Vehicle Entry → Assign Slot → Save │
-//  │ 2. Vehicle Exit  → Calc Charges → Free│
-//  │ 3. View Parked Vehicles               │
-//  │ 4. View Available Slots               │
-//  │ 5. Generate Report                    │
-//  │ 6. Exit Program
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+class Vehicle{
+    private:
+        int vehicleNo;
+        string ownerName;
+        string status;
+        time_t entryTime;
+        time_t exitTime;
+    public:
+        void parkingVehicle(){
 
- #include <iostream>
- using namespace std;
- int main(){
-    
- }
+        }
+        void exitVehicle(){
+         
+        }
+};
+class ParkingSlot{
+    private:
+        int slotId;
+        string status;
+    public:
+        void freeSlot(){
+
+        }
+        void checkAvailability(){
+
+        }
+};
+class ParkingSystem{
+    private:
+        vector<Vehicle> vehicles;
+        vector<ParkingSlot> slots;
+    public:
+        void addVehicle(){
+
+        }
+        void removeVehicle(){
+
+        }
+        void viewVehicle(){
+
+        }
+        void ViewSlot(){
+
+        }
+        void generateReport(){
+
+        }
+};
+int main(){
+   int choice;
+   cout << "1. Park Vehicle"<<endl;
+   cout << "2. Exit Vehicle"<<endl;
+   cout << "3. View Parked Vehicles"<<endl;
+   cout << "4. View Available Slots"<<endl;
+   cout << "5. Generate Report"<<endl;
+   cout << "0. Exit"<<endl;
+   cout << "Enter choice: "<<endl;
+   cin  >> choice;
+   switch(choice){
+       case 1:
+       case 2:
+       case 3:
+       case 4:
+       case 5:
+       case 0:
+    }
+}
