4ad6ecd12c1c1fefc655cc640d1bc9b202cb04fe
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-21 12:27:21 +0530

    file name changed

--- Parking_Management_System.cpp
+++ Parking_Management_System.cpp
@@ -9,11 +9,14 @@ class Vehicle{
         string status;
         time_t entryTime;
         time_t exitTime;
+        double duration;
     public:
+        Vehicle(){}
         Vehicle(string vehicleNo,string ownerName){
             this->vehicleNo=vehicleNo;
             this->ownerName=ownerName;
             entryTime=time(0);
+            
         }
         string getVehicleNo(){
             return vehicleNo; 
@@ -28,10 +31,16 @@ class Vehicle{
 
         }
         void exitVehicle(){
-         
+            exitTime=time(0);
+            
+            double charges=calculateCharges();
+            calculateDuration();
+        }
+        void calculateDuration(){
+            duration=difftime(exitTime,entryTime)/60;
         }
         virtual double calculateCharges(){
-            return 20;
+            return duration*50;
         }
 };
 class ParkingSlot{
@@ -54,11 +63,25 @@ class ParkingSystem{
         vector<Vehicle*> vehicles;
         vector<ParkingSlot> slots;
     public:
-        ParkingSystem(string vno){
-            if
-        }
-        void addVehicle(){
-
+        
+        void addVehicle(string vno,string ownerName){
+            for(int i=0;i<vehicles.size();i++){
+                if(vehicles[i]->getVehicleNo()==vno){
+                        cout << "Welcome back " << vehicles[i]->getOwnerName() << endl;
+                        Vehicle* v=new Vehicle(vno,ownerName);
+                        vehicles.push_back(v);
+                        cout << "Your vehicle number is " << vno << endl;
+                        time_t entry = v->getEntryTime();
+                        cout << "Your entry time is " << ctime(&entry) << endl;
+                        return;
+                    }    
+                }                    
+            Vehicle* v=new Vehicle(vno,ownerName);
+            vehicles.push_back(v);
+            cout << "Vehicle added successfully"<<endl;
+            cout << "Your vehicle number is " << vno << endl;
+            time_t entry = v->getEntryTime();
+            cout << "Your entry time is " << ctime(&entry) << endl;
         }
         void removeVehicle(string vno){
             for(int i=0;i<vehicles.size();i++){
@@ -72,8 +95,14 @@ class ParkingSystem{
             cout << "Vehicle not found"<<endl;
         }
         void viewVehicle(){
-
-        }
+            for(int i=0;i<vehicles.size();i++){
+                cout << "Vehicle Number: " << vehicles[i]->getVehicleNo() << endl;
+                cout << "Owner Name: " << vehicles[i]->getOwnerName() << endl;
+                time_t entry = vehicles[i]->getEntryTime();
+                cout << "Entry Time: " << ctime(&entry) << endl;
+                cout << "------------------------" << endl;
+            }
+        }   
         void ViewSlot(){
 
         }
@@ -82,10 +111,10 @@ class ParkingSystem{
         }
 };
 int main(){
-    {
-        
+        ParkingSystem ps;
         int type, choice;
         string vno, ownerName;
+        do{
         cout << "1. Park Vehicle" << endl;
         cout << "2. Exit Vehicle" << endl;
         cout << "3. View Parked Vehicles" << endl;
@@ -100,18 +129,26 @@ int main(){
                 cin >> ownerName;
                 cout << "Enter your vehicle number: " << endl;
                 cin >> vno;
-                Vehicle* v = new Vehicle(vno, ownerName);
+                ps.addVehicle(vno, ownerName);
                 break;
             case 2:
                 cout << "Enter your vehicle number: " << endl;
                 cin >> vno;
-                ParkingSystem ps(vno);
+                ps.removeVehicle(vno);
                 break;
             case 3:
-            case 4:
-            case 5:
-            case 0:
+                ps.viewVehicle();
                 break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 0:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
         }
-    }
+    }    while(choice!=0);
+    return 0;   
 }
