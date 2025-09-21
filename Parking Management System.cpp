5e80d6d8830a203daed580380ae8d458e4deeb20
Omjadhav13 <omjadhav1303@gmail.com>
2025-09-21 10:58:33 +0530

    removed car and bike

--- Parking Management System.cpp
+++ Parking Management System.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 class Vehicle{
     private:
-        int vehicleNo;
+        string vehicleNo;
         string ownerName;
         string status;
         time_t entryTime;
         time_t exitTime;
     public:
-        Vehicle(int vehicleNo,string ownername){
+        Vehicle(string vehicleNo,string ownerName){
             this->vehicleNo=vehicleNo;
             this->ownerName=ownerName;
             entryTime=time(0);
         }
-        int getVehicleNo(){
+        string getVehicleNo(){
             return vehicleNo; 
         }
         string getOwnerName(){
@@ -34,22 +34,6 @@ class Vehicle{
             return 20;
         }
 };
-
-class car:public Vehicle{
-    public:
-        double calculateCharges(time_t time){
-            
-            
-        }
-};
-
-class Bike:public Vehicle{
-    public: 
-        double calculateCharges() override{
-            return 20;
-        }
-};
-
 class ParkingSlot{
     private:
         int slotId;
@@ -70,14 +54,22 @@ class ParkingSystem{
         vector<Vehicle*> vehicles;
         vector<ParkingSlot> slots;
     public:
-        ParkingSystem(int totalslots){
-            
+        ParkingSystem(string vno){
+            if
         }
         void addVehicle(){
 
         }
-        void removeVehicle(){
-
+        void removeVehicle(string vno){
+            for(int i=0;i<vehicles.size();i++){
+                if(vehicles[i]->getVehicleNo()==vno){
+                    vehicles[i]->exitVehicle();
+                    vehicles.erase(vehicles.begin()+i);
+                    cout << "Vehicle removed successfully"<<endl;
+                    return;
+                }
+            }
+            cout << "Vehicle not found"<<endl;
         }
         void viewVehicle(){
 
@@ -90,44 +82,36 @@ class ParkingSystem{
         }
 };
 int main(){
-    
-    int type,choice;
-    string vno,ownerName;
-    cout << "1. Park Vehicle"<<endl;
-    cout << "2. Exit Vehicle"<<endl;
-    cout << "3. View Parked Vehicles"<<endl;
-    cout << "4. View Available Slots"<<endl;
-    cout << "5. Generate Report"<<endl;
-    cout << "0. Exit"<<endl;
-    cout << "Enter choice: "<<endl;
-    cin  >> choice;
-    switch(choice){
-        case 1:
-            cout <<"Enter your name: "<<endl;
-            cin >> ownerName;
-            cout << "Enter your vehicle number: "<< endl;
-            cin >> vno;
-            cout << "Enter your vehicle type: 1. Car or 2. Bike"<<endl;
-            cin >> type;
-            if(type==1){
-                Vehicle *v=new car();
-            }
-            else if(type==2){
-                Vehicle *v=new Bike();
-            }
-            else{
-                cout << "Invalid vehicle type"<<endl;
-            }   
-            break;
-        case 2:
-            cout << "Enter your vehicle number: "<< endl;
-            cin >> vno;
-            ParkingSystem ps;
-            ps.removeVehicle(vno);
-            break;
-        case 3:
-        case 4:
-        case 5:
-        case 0:
+    {
+        
+        int type, choice;
+        string vno, ownerName;
+        cout << "1. Park Vehicle" << endl;
+        cout << "2. Exit Vehicle" << endl;
+        cout << "3. View Parked Vehicles" << endl;
+        cout << "4. View Available Slots" << endl;
+        cout << "5. Generate Report" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter choice: " << endl;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "Enter your name: " << endl;
+                cin >> ownerName;
+                cout << "Enter your vehicle number: " << endl;
+                cin >> vno;
+                Vehicle* v = new Vehicle(vno, ownerName);
+                break;
+            case 2:
+                cout << "Enter your vehicle number: " << endl;
+                cin >> vno;
+                ParkingSystem ps(vno);
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 0:
+                break;
+        }
     }
 }
