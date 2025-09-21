71d14e5f3b6a708063b025f1d7fb5bbbc4d1922f
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2025-09-21 21:17:06 +0530

    Update Parking_Management_System.cpp

--- Parking_Management_System.cpp
+++ Parking_Management_System.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <windows.h>
+#include <string>
+#include <mysql>
 using namespace std;
+
 class Vehicle{
     private:
         string vehicleNo;
@@ -15,8 +19,7 @@ class Vehicle{
         Vehicle(string vehicleNo,string ownerName){
             this->vehicleNo=vehicleNo;
             this->ownerName=ownerName;
-            entryTime=time(0);
-            
+            entryTime=time(0);        
         }
         string getVehicleNo(){
             return vehicleNo; 
@@ -28,13 +31,14 @@ class Vehicle{
             return entryTime;
         }
         void parkingVehicle(){
-
+            entryTime=time(0);
         }
         void exitVehicle(){
-            exitTime=time(0);
-            
-            double charges=calculateCharges();
+            exitTime = time(0);
             calculateDuration();
+            double charges = calculateCharges();
+            cout << "Parking duration: " << duration << " minutes" << endl;
+            cout << "Total bill: " << charges << " Rs." << endl;
         }
         void calculateDuration(){
             duration=difftime(exitTime,entryTime)/60;
@@ -46,76 +50,105 @@ class Vehicle{
 class ParkingSlot{
     private:
         int slotId;
-        string status;
+        bool isOccupied;
     public:
         ParkingSlot(int slotId){
-            this->slotId=slotId;
+            this->slotId = slotId;
+            isOccupied = false;
+        }
+        void occupySlot(){
+            isOccupied = true;
         }
         void freeSlot(){
-
+            isOccupied = false;
         }
         void checkAvailability(){
-            
+            if(!isOccupied){
+                cout << "Slot " << slotId << " is free" << endl;
+            } else{
+                cout << "Slot " << slotId << " is occupied" << endl;
+            }
+        }
+        bool getStatus(){
+            return isOccupied;
         }
 };
+
 class ParkingSystem{
     private:
         vector<Vehicle*> vehicles;
         vector<ParkingSlot> slots;
+        int totalSlots;
+
     public:
-        
-        void addVehicle(string vno,string ownerName){
-            for(int i=0;i<vehicles.size();i++){
-                if(vehicles[i]->getVehicleNo()==vno){
-                        cout << "Welcome back " << vehicles[i]->getOwnerName() << endl;
-                        Vehicle* v=new Vehicle(vno,ownerName);
-                        vehicles.push_back(v);
-                        cout << "Your vehicle number is " << vno << endl;
-                        time_t entry = v->getEntryTime();
-                        cout << "Your entry time is " << ctime(&entry) << endl;
-                        return;
-                    }    
-                }                    
-            Vehicle* v=new Vehicle(vno,ownerName);
-            vehicles.push_back(v);
-            cout << "Vehicle added successfully"<<endl;
-            cout << "Your vehicle number is " << vno << endl;
-            time_t entry = v->getEntryTime();
-            cout << "Your entry time is " << ctime(&entry) << endl;
+        // Constructor
+        ParkingSystem(int n = 10){  
+            totalSlots = n;
+            for(int i=1;i<=n;i++){
+                ParkingSlot ps(i);
+                slots.push_back(ps);
+            }
         }
+
+        void addVehicle(string vno,string ownerName){
+            int assignedSlot = -1;
+            for(int i=0;i<slots.size();i++){
+                if(!slots[i].getStatus()){
+                    assignedSlot = i;
+                    slots[i].occupySlot();
+                    break;
+                }
+            }
+            if(assignedSlot == -1){
+                cout << "No slots available!" << endl;
+                return;
+            }
+            Vehicle* v = new Vehicle(vno, ownerName);
+            vehicles.push_back(v);
+            cout << "Vehicle parked in slot " << assignedSlot + 1 << endl;
+            time_t entry = v->getEntryTime();
+            cout << "Entry Time: " << ctime(&entry) << endl;
+        }
+
         void removeVehicle(string vno){
             for(int i=0;i<vehicles.size();i++){
-                if(vehicles[i]->getVehicleNo()==vno){
+                if(vehicles[i]->getVehicleNo() == vno){
                     vehicles[i]->exitVehicle();
-                    vehicles.erase(vehicles.begin()+i);
-                    cout << "Vehicle removed successfully"<<endl;
+                    slots[i].freeSlot();  // consider storing slot in Vehicle
+                    vehicles.erase(vehicles.begin() + i);
+                    cout << "Vehicle removed successfully" << endl;
                     return;
                 }
             }
-            cout << "Vehicle not found"<<endl;
+            cout << "Vehicle not found" << endl;
         }
+
         void viewVehicle(){
+            if(vehicles.size()==0){
+                cout << "No vehicles parked, All slots are free" << endl;
+                return;
+            }
             for(int i=0;i<vehicles.size();i++){
-                if(vehicles.size()==0){
-                    cout << "No vehicles parked, All slots are free"<<endl;
-                    return;
-                }
-                else{
-                    cout << "Vehicle Number: " << vehicles[i]->getVehicleNo() << endl;
-                    cout << "Owner Name: " << vehicles[i]->getOwnerName() << endl;
-                    time_t entry = vehicles[i]->getEntryTime();
-                    cout << "Entry Time: " << ctime(&entry) << endl;
-                    cout << "------------------------" << endl;
-                }
+                cout << "Vehicle Number: " << vehicles[i]->getVehicleNo() << endl;
+                cout << "Owner Name: " << vehicles[i]->getOwnerName() << endl;
+                time_t entry = vehicles[i]->getEntryTime();
+                cout << "Entry Time: " << ctime(&entry) << endl;
+                cout << "------------------------" << endl;
             }
         }   
+
         void ViewSlot(){
-
+            for(int i=0;i<slots.size();i++){
+                slots[i].checkAvailability();
+            }
         }
-        void generateReport(){
 
+        void generateReport(){
+            cout << "Total vehicles parked: " << vehicles.size() << endl;
+            cout << "Total slots available: " << slots.size()-vehicles.size() << endl;
         }
 };
+
 int main(){
         ParkingSystem ps;
         int type, choice;
@@ -140,14 +173,16 @@ int main(){
             case 2:
                 cout << "Enter your vehicle number: " << endl;
                 cin >> vno;
-                ps.removeVehicle(vno);
+                ps.removeVehicle(vno);  
                 break;
             case 3:
                 ps.viewVehicle();
                 break;
             case 4:
+                ps.ViewSlot();
                 break;
             case 5:
+                ps.generateReport();
                 break;
             case 0:
                 cout << "Exiting..." << endl;
